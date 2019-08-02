eb31642330821463fd5f789926b5a263da7aff9d
snibo13 <snibo13@gmail.com>
2019-08-01 23:16:37 -0500

    Modified MathString for double digit operations

--- lib/MathString.dart
+++ lib/MathString.dart
@@ -1,52 +1,86 @@
-/*
-Created by Sidney Nimako-Boateng
-Four Fours App
-TODO: Add square root function
- */
-
-/*
-Parses a string equation to a numerical value
- */
 int parseEquation(String equation) {
-  //Stores values in the cases of parentheses
   var values = [];
-  //Actual numerical value to be returned
   int val = 0;
-  //Efficiency if the equation does not contain any grouping symbols
   if (!equation.contains("(")) {
     return oop(equation);
   }
-  //Loop through if the equation does have parentheses adding the calculated
-  // values to the values array
   while (equation.contains('(')) {
-    //Call the calculate function on the sub equation within the parentheses
     values.add(oop(equation.substring(equation.indexOf('(')+1, equation.indexOf(')'))));
-    //Modify the equation to remove the parentheses and their contents
-    equation = equation.replaceRange(0, equation.indexOf(')')+1," ");
+    var temp = values[values.length-1];
+    equation = equation.replaceRange(equation.indexOf('('), equation.indexOf(')')+1,"$temp");
   }
-  //If their is an operation to be done outside of the parentheses
-  if(!(equation.substring(0,1) is num)) {
-    //Calculate the value of the integers within the values array
-    for (int i = 0; i < values.length; i++) {
-      val += values[i];
-    }
-    //Return the value of the new equation re-calling parseEquation
-    return parseEquation("$val" + equation);
+  if(int.tryParse(equation) == null) {
+    return parseEquation(equation);
   } else {
-    //If there is no other operations to be done sum the values and return
     for (int i = 0; i < values.length; i++) {
       val += values[i];
     }
-    print(equation);
     return val;
   }
 
 }
-/*
-  Calculates the value of an equation
-  param: equation to be calculated
- */
-int calculate(String subEquation) {
+
+int calculate(int opIndex, String subEquation) {
+  //Remove all spaces
+  subEquation = subEquation.replaceAll(" ","");
+  //Value to be returned
+  int val = int.parse(subEquation.substring(0,opIndex));
+  //Iterate through the subEquation performing operations as seen
+  while(subEquation.length >= 1) {
+    switch (subEquation.substring(0,1)) {
+      case '÷':
+        val ~/= int.parse(subEquation.substring(opIndex-1));
+        break;
+      case 'x':
+        val *= int.parse(subEquation.substring(opIndex));
+        break;
+      case '+':
+        val += int.parse(subEquation.substring(opIndex));
+        break;
+      case '-':
+        val -= int.parse(subEquation.substring(opIndex));
+        break;
+    }
+    //Remove the first character in the equation
+    subEquation = subEquation.substring(1);
+  }
+
+  //Return the computed value
+  return val;
+}
+
+int oop(String subEquation) {
+  var values = [];
+  int val = 0;
+  while (subEquation.contains('\u221a')) {
+    subEquation = subEquation.replaceRange(subEquation.indexOf('\u221a'), subEquation.indexOf('\u221a')+2,"2");
+  }
+  subEquation = oopPair(subEquation,"\u00F7", "x");
+  subEquation = oopPair(subEquation,"+", "-");
+  return int.parse(subEquation);
+}
+
+List digOp(String op, String equation) {
+  List vals = [];
+  int startIndex = 1;
+  int opIndex = equation.indexOf(op);
+  int endIndex = opIndex + 1;
+  if (opIndex - 2 >= 0 && int.tryParse(equation.substring(opIndex -2, opIndex)) != null) {
+    startIndex = opIndex - 2;
+  }
+  if (opIndex + 3 <= equation.length && int.tryParse(equation.substring(opIndex+1, opIndex+3)) != null) {
+    endIndex = opIndex + 3;
+  }
+  return [startIndex, endIndex];
+}
+bool isDoubleDigit(String s, int index) {
+  if (int.tryParse(s.substring(index, index+3)) != null) {
+    return true;
+  }
+  return false;
+}
+
+int calculate2(String subEquation) {
   //Remove all spaces
   subEquation = subEquation.replaceAll(" ","");
   //Value to be returned
@@ -88,71 +122,52 @@ int calculate(String subEquation) {
   return val;
 }
 
-int oop(String subEquation) {
-  var values = [];
-  int val = 0;
-  while (subEquation.contains('\u221a')) {
-    subEquation = subEquation.replaceRange(subEquation.indexOf('\u221a'), subEquation.indexOf('\u221a')+2,"2");
-  }
-  while(subEquation.contains("\u00F7") || subEquation.contains("x")) {
-    //Contains Multiplication
-    if (subEquation.contains("x")) {
-      //Both signs
-      if (subEquation.contains("\u00F7")) {
-        //Multiplication before division
-        if(subEquation.indexOf("x") < subEquation.indexOf("\u00F7")) {
-          int temp = calculate(subEquation.substring(subEquation.indexOf("x")-1, subEquation.indexOf("x")+2));
-          subEquation = subEquation.replaceRange(subEquation.indexOf("x")-1, subEquation.indexOf("x")+2,"$temp");
-          print(subEquation);
+String oopPair(String subEquation, String char1, String char2) {
+  int temp;
+  while(subEquation.contains(char1) || subEquation.contains(char2)) {
+    if (subEquation.contains(char2)) {
+      if (subEquation.contains(char1)) {
+        if(subEquation.indexOf(char2) < subEquation.indexOf(char1)) {
+          List indeces = digOp(char2, subEquation);
+          temp = calculate(subEquation.indexOf(char2), subEquation.substring(indeces[0], indeces[1]));
+          subEquation = subEquation.replaceRange(indeces[0],indeces[1],"$temp");
         }
-        //Division before Multiplication
         else {
-          int temp = calculate(subEquation.substring(subEquation.indexOf("\u00F7")-1, subEquation.indexOf("\u00F7")+2));
-          subEquation = subEquation.replaceRange(subEquation.indexOf("\u00F7")-1, subEquation.indexOf("\u00F7")+2, "$temp");
-          print(subEquation);
+          List indeces = digOp(char1, subEquation);
+          temp = calculate(subEquation.indexOf(char1), subEquation.substring(indeces[0], indeces[1]));
+          subEquation = subEquation.replaceRange(indeces[0],indeces[1],"$temp");
         }
       }
-      //Only multiplication
       else {
-        int temp = calculate(subEquation.substring(subEquation.indexOf("x")-1, subEquation.indexOf("x")+2));
-        subEquation = subEquation.replaceRange(subEquation.indexOf("x")-1, subEquation.indexOf("x")+2,"$temp");
-        print(subEquation);
+        if (subEquation.length > 3) {
+          List indeces = digOp(char2, subEquation);
+
+          temp = calculate(subEquation.indexOf(char2), subEquation.substring(indeces[0], indeces[1]));
+          subEquation = subEquation.replaceRange(indeces[0],indeces[1],"$temp");
+        } else {
+          temp = calculate2(subEquation.substring(subEquation.indexOf(char2)-1, subEquation.indexOf(char2)+2));
+          subEquation = subEquation.replaceRange(subEquation.indexOf(char2)-1, subEquation.indexOf(char2)+2,"$temp");
+        }
       }
     }
-    //Only has division
     else {
-      int temp = calculate(subEquation.substring(subEquation.indexOf("\u00F7")-1, subEquation.indexOf("\u00F7")+2));
-      subEquation = subEquation.replaceRange(subEquation.indexOf("\u00F7")-1, subEquation.indexOf("\u00F7")+2,"$temp");
-      print(subEquation);
+      List indeces = digOp(char1, subEquation);
+      temp = calculate(subEquation.indexOf(char1), subEquation.substring(indeces[0], indeces[1]));
+      subEquation = subEquation.replaceRange(indeces[0],indeces[1],"$temp");
     }
   }
-  while(subEquation.contains("-") || subEquation.contains("+")) {
-    //Contains addition
-    if (subEquation.contains("+")) {
-      //Both signs
-      if (subEquation.contains("-")) {
-        //Addition before subtraction
-        if(subEquation.indexOf("+") < subEquation.indexOf("-")) {
-          int temp = calculate(subEquation.substring(subEquation.indexOf("+")-1, subEquation.indexOf("+")+2));
-          subEquation = subEquation.replaceRange(subEquation.indexOf("+")-1, subEquation.indexOf("+")+2,"$temp");
-        }
-        //Subtraction before addition
-        else {
-          int temp = calculate(subEquation.substring(subEquation.indexOf("-")-1, subEquation.indexOf("-")+2));
-          subEquation = subEquation.replaceRange(subEquation.indexOf("-")-1, subEquation.indexOf("-")+2, "$temp");
-        }
-      }
-      //Only addition
-      else {
-        int temp = calculate(subEquation.substring(subEquation.indexOf("+")-1, subEquation.indexOf("+")+2));
-        subEquation = subEquation.replaceRange(subEquation.indexOf("+")-1, subEquation.indexOf("+")+2,"$temp");
-      }
-    }
-    //Only has subtraction
-    else {
-      int temp = calculate(subEquation.substring(subEquation.indexOf("-")-1, subEquation.indexOf("-")+2));
-      subEquation = subEquation.replaceRange(subEquation.indexOf("-")-1, subEquation.indexOf("-")+2,"$temp");
-    }
-  }
-  return int.parse(subEquation);
+  return subEquation;
+}
+String regex(String string) {
+  RegExp re = new RegExp('\\d{0,2}');
+  Iterable terms = re.allMatches(string);
+  int startIndex = terms.first.start;
+  int endIndex = terms.last.end;
+  return string.substring(startIndex, endIndex);
+}
+
+Iterable regex1(String string) {
+  RegExp re = new RegExp('\\d{0,2}');
+  Iterable terms = re.allMatches(string);
+  return terms;
 }
