fa10991db8460d6ac9c164a79ded21fcceff5f36
Sidney Nimako <snibo13@gmail.com>
2019-12-22 12:52:43 -0600

    Added decimal functionality

--- lib/MathEngine.dart
+++ lib/MathEngine.dart
@@ -1,19 +1,34 @@
 import 'dart:collection';
 import 'dart:math';
-
+//TODO: Implement place value multiplication for numbers greater than 9
 String postfix(String equation) {
   var operatorPrecedenceInStack = {"(":0,"!":7,"²":5,"√":5,"x":4,"÷":4,"+":2,"-":2};
   var operatorPrecedenceToStack = {"(":10,"!":8,"²":6,"√":6,"x":3,"÷":3,"+":1,"-":1}; //Out
   ListQueue operators = new ListQueue(); //Functioning as a queue
   ListQueue output = new ListQueue(); //Functioning as a stack
+  equation = equation.replaceAll(" ","");
   while(equation.length > 0) {
+    print(equation);
+    //Adding double digit functionality
+    int numIndex = 0;
     //If the first character is a number
-    if (int.tryParse(equation.substring(0,1)) != null) {
+    //Accomodates for numbers of variable lengths
+    if (equation.length >= 3 && equation.substring(0,3).contains(".") && double.tryParse(equation.substring(0,3)) != null) {
+      numIndex = 3;
+    }
+    else if (equation.length >= 2 && equation.substring(0,2).contains(".") && double.tryParse(equation.substring(0,2)) != null) {
+      numIndex = 2;
+    }  else if (double.tryParse(equation.substring(0,1)) != null) {
+      numIndex = 1;
+    }
+    if (numIndex != 0) {
+      print(equation.substring(0,numIndex));
       //Queue push
-      output.addLast(equation.substring(0,1));
+      output.addLast(equation.substring(0,numIndex));
       //Remove the first string from the equation (SIDE EFFECT)
-      equation = equation.substring(1);
+      equation = equation.substring(numIndex);
     } else {
+      print(equation.substring(0,1));
       //For the first operator
       if (operators.isEmpty) {
         //Stack push
@@ -23,7 +38,12 @@ String postfix(String equation) {
       } else { //If not the first operator
         //If its a closing parenthese
         if(equation.substring(0,1) == ")") {
-          do {output.addLast(operators.removeFirst());}
+          print(operators);
+          do {
+            output.addLast(operators.removeFirst());
+            print("out $output");
+            print("Ops $operators");
+          }
           while (operators.first != "(");
           operators.removeFirst();
           //Remove the first string from the equation (SIDE EFFECT)
@@ -38,6 +58,7 @@ String postfix(String equation) {
         }
         else {
           while(operatorPrecedenceToStack[equation.substring(0,1)] < operatorPrecedenceInStack[operators.first] && operators.isNotEmpty) {
+            //Pop all operators with higher precedence to the output
             output.addLast(operators.removeFirst());
             if(operators.isEmpty) {
               break;
@@ -60,28 +81,38 @@ String postfix(String equation) {
   return out;
 }
 
-int calculatePostfix(String equation) {
+double calculatePostfix(String equation) {
   ListQueue terms = new ListQueue(); //Functioning as a stack
-  int ret = 0;
-
+  int symbolIndex = 0;
+  equation = equation.replaceAll(" ","");
+  print(equation);
+  //Compensates for numbers of variable length
   while(equation.length > 0) {
-    String symbol = equation.substring(0,1);
-    if (int.tryParse(symbol) != null) {
-      terms.addLast(int.parse(symbol));
+    //Three digits only possible with decimals
+    if(equation.length >= 3 && (equation.substring(0,3)).contains(".") && double.tryParse(equation.substring(0,3)) != null) {
+      symbolIndex = 3;
+    } else if(equation.length >=2 && equation.substring(0,2).contains(".") && double.tryParse(equation.substring(0,2)) != null) {
+      symbolIndex = 2;
+    } else {
+      symbolIndex = 1;
+    }
+    String symbol = equation.substring(0,symbolIndex);
+    if (double.tryParse(symbol) != null) {
+      terms.addLast(double.parse(symbol));
       //Remove the first string from the equation (SIDE EFFECT)
-      equation = equation.substring(1);
+      equation = equation.substring(symbolIndex);
     }
     else
     {
       switch(symbol){
         case "x":
-          int t1 = terms.removeLast();
-          int t2 = terms.removeLast();
+          double t1 = terms.removeLast();
+          double t2 = terms.removeLast();
           terms.addLast(t1*t2);
           break;
         case "÷":
-          int t1 = terms.removeLast();
-          int t2 = terms.removeLast();
+          double t1 = terms.removeLast();
+          double t2 = terms.removeLast();
           if (t1 < t2) {
             terms.addLast(t2 ~/ t1);
           } else {
@@ -89,13 +120,13 @@ int calculatePostfix(String equation) {
           }
           break;
         case "+":
-          int t1 = terms.removeLast();
-          int t2 = terms.removeLast();
+          double t1 = terms.removeLast();
+          double t2 = terms.removeLast();
           terms.addLast(t1+t2);
           break;
         case "-":
-          int t2 = terms.removeLast();
-          int t1 = terms.removeLast();
+          double t2 = terms.removeLast();
+          double t1 = terms.removeLast();
           if (t2 > t1) {
             terms.addLast(t2 - t1);
           }else {
@@ -103,34 +134,34 @@ int calculatePostfix(String equation) {
           }
           break;
         case "√":
-          int t = terms.removeLast();
-          terms.addLast(sqrt(t).toInt());
+          double t = terms.removeLast();
+          terms.addLast(sqrt(t).toDouble());
           break;
         case "²":
-          int t = terms.removeLast();
+          double t = terms.removeLast();
           terms.addLast(t * t);
           break;
         case "!":
-          int t = terms.removeLast();
+          double t = terms.removeLast();
           terms.addLast(factorial(t));
           break;
       }
 
       //Remove the first string from the equation (SIDE EFFECT)
-      equation = equation.substring(1);
+      equation = equation.substring(symbolIndex);
     }
   }
   return terms.removeFirst();
 }
 
-int factorial(int f) {
+double factorial(double f) {
   if (f == 0) {
     return 1;
   }
   return f * factorial(f-1);
 }
 
-int mathEngine(String s) {
-  print(postfix(s));
+double mathEngine(String s) {
+  //print(postfix(s));
   return calculatePostfix(postfix(s));
 }
